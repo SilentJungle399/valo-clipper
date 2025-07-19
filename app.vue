6520ed007b79b6cda52d8ba535bfa5ac045d1523
SilentJungle399 <vardhan.harsh4041@gmail.com>
2025-07-19 19:11:31 +0530

    feat: input form

--- app.vue
+++ app.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<n-config-provider :theme="darkTheme">
+		<n-message-provider>
+			<div class="container">
+				<InputForm></InputForm>
+			</div>
+		</n-message-provider>
+	</n-config-provider>
 </template>
+
+<script setup lang="ts">
+import { darkTheme, NConfigProvider, NMessageProvider, useMessage } from "naive-ui";
+
+// onMounted(() => {
+// 	window.$message = useMessage();
+// });
+</script>
+
+<style>
+body {
+	background-color: #1e1e1e;
+	color: white;
+}
+
+.container {
+	display: flex;
+	justify-content: center;
+	margin-top: 50px;
+}
+</style>
---
+++ components/InputForm.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="input-form">
+		<div class="input-group">
+			<span class="input-label">Youtube URL</span>
+			<n-input-group>
+				<n-input
+					:style="{ width: '50rem' }"
+					placeholder="https://youtube.com/..."
+					v-model:value="data.youtubeUrl"
+				/>
+				<n-button type="primary" @click="handleYoutube">
+					<n-icon size="20">
+						<Search />
+					</n-icon>
+				</n-button>
+			</n-input-group>
+		</div>
+		<div class="input-group">
+			<span class="input-label">Player Info</span>
+			<n-input-group>
+				<n-input
+					:style="{ width: '10rem' }"
+					placeholder="Player name"
+					v-model:value="data.playerName"
+				/>
+				<n-input-group-label>#</n-input-group-label>
+				<n-input
+					:style="{ width: '5rem' }"
+					placeholder="Tag"
+					v-model:value="data.playerTag"
+				/>
+				<n-button type="primary" @click="handlePlayerInfo">
+					<n-icon size="20">
+						<Search />
+					</n-icon>
+				</n-button>
+			</n-input-group>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { NInput, NInputGroup, NInputGroupLabel, NButton, NIcon, useMessage } from "naive-ui";
+import { Search } from "@vicons/ionicons5";
+
+const message = useMessage();
+const data = reactive({
+	youtubeUrl: "",
+	playerName: "",
+	playerTag: "",
+});
+
+const handleYoutube = () => {
+	if (!data.youtubeUrl) {
+		message.error("Please enter a YouTube URL.");
+		return;
+	}
+	message.success("YouTube URL submitted: " + data.youtubeUrl);
+};
+
+const handlePlayerInfo = () => {
+	if (!data.playerName || !data.playerTag) {
+		message.error("Please enter both player name and tag.");
+		return;
+	} else if (
+		data.playerName.length < 3 ||
+		data.playerName.length > 16 ||
+		data.playerTag.length < 3 ||
+		data.playerTag.length > 5
+	) {
+		message.error("Please enter valid player info.");
+		return;
+	}
+};
+</script>
+
+<style>
+.input-form {
+	display: flex;
+	gap: 10px;
+}
+
+.input-group {
+	display: flex;
+	flex-direction: column;
+	gap: 2px;
+}
+
+.input-label {
+	color: #9ca3af;
+	font-weight: bold;
+	font-size: 14px;
+}
+</style>
